b867d3c0b905d350d208ccf1573b4ddb8486d600
@jade-seal-of-wisdom <3027210896@qq.com>
2025-09-23 21:51:05 +0800

    strings3

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,19 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    // 移除字符串两端的空白字符并转换为String类型
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // 将" world!"添加到字符串末尾
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    // 将字符串中的"cars"替换为"balloons"
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
