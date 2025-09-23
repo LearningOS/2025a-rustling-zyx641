5d7001106e64c1bb4aea25f567fb652ae215a266
@jade-seal-of-wisdom <3027210896@qq.com>
2025-09-23 18:21:07 +0800

    9

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -37,7 +37,18 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
+        // 将新元素添加到向量末尾
+        self.count += 1;
+        
+        // 如果向量容量不足，扩展它
+        if self.count < self.items.len() {
+            self.items[self.count] = value;
+        } else {
+            self.items.push(value);
+        }
+        
+        // 执行上浮操作，维护堆的性质
+        self.sift_up(self.count);
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -57,8 +68,60 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        // 检查是否有子节点
+        if !self.children_present(idx) {
+            return 0; // 没有子节点时返回0（无效索引）
+        }
+        
+        let left = self.left_child_idx(idx);
+        let right = self.right_child_idx(idx);
+        
+        // 检查是否有右子节点
+        if right > self.count {
+            return left; // 只有左子节点时返回左子节点索引
+        }
+        
+        // 根据比较器选择"较小"的子节点
+        let comparator = self.comparator;
+        if comparator(&self.items[left], &self.items[right]) {
+            left
+        } else {
+            right
+        }
+    }
+    
+    // 上浮操作：将指定索引的元素向上移动到正确位置
+    fn sift_up(&mut self, mut idx: usize) {
+        let comparator = self.comparator;
+        
+        // 当元素不是根节点且满足比较条件时，继续上浮
+        while idx > 1 {
+            let parent = self.parent_idx(idx);
+            if comparator(&self.items[idx], &self.items[parent]) {
+                // 交换元素
+                self.items.swap(idx, parent);
+                idx = parent;
+            } else {
+                break; // 已经在正确位置
+            }
+        }
+    }
+    
+    // 下沉操作：将指定索引的元素向下移动到正确位置
+    fn sift_down(&mut self, mut idx: usize) {
+        let comparator = self.comparator;
+        
+        // 当元素有子节点时，继续下沉
+        while self.children_present(idx) {
+            let child = self.smallest_child_idx(idx);
+            if comparator(&self.items[child], &self.items[idx]) {
+                // 交换元素
+                self.items.swap(idx, child);
+                idx = child;
+            } else {
+                break; // 已经在正确位置
+            }
+        }
     }
 }
 
@@ -79,13 +142,34 @@ where
 
 impl<T> Iterator for Heap<T>
 where
-    T: Default,
+    T: Default + Clone,
 {
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        // 如果堆为空，返回None
+        if self.is_empty() {
+            return None;
+        }
+        
+        // 保存堆顶元素（索引1处的元素）
+        let result = std::mem::replace(&mut self.items[1], T::default());
+        
+        // 将最后一个元素移到堆顶
+        if self.count > 1 {
+            self.items[1] = self.items[self.count].clone();
+        }
+        
+        // 减少计数
+        self.count -= 1;
+        
+        // 如果堆不为空，执行下沉操作
+        if !self.is_empty() {
+            self.sift_down(1);
+        }
+        
+        // 返回保存的堆顶元素
+        Some(result)
     }
 }
 
@@ -95,7 +179,7 @@ impl MinHeap {
     #[allow(clippy::new_ret_no_self)]
     pub fn new<T>() -> Heap<T>
     where
-        T: Default + Ord,
+        T: Default + Ord + Clone,
     {
         Heap::new(|a, b| a < b)
     }
@@ -107,7 +191,7 @@ impl MaxHeap {
     #[allow(clippy::new_ret_no_self)]
     pub fn new<T>() -> Heap<T>
     where
-        T: Default + Ord,
+        T: Default + Ord + Clone,
     {
         Heap::new(|a, b| a > b)
     }
