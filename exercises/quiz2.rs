ffd5c0d3a8884aa326cf9dd3c54727081dc4514d
@jade-seal-of-wisdom <3027210896@qq.com>
2025-09-23 22:37:26 +0800

    quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -31,12 +31,27 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    // 完成函数签名
+    pub fn transformer(input: &Vec<(String, Command)>) -> Vec<String> {
+        // 完成输出声明
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            // 根据不同的命令执行不同的操作
+            match command {
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                },
+                Command::Trim => {
+                    output.push(string.trim().to_string());
+                },
+                Command::Append(n) => {
+                    let mut new_string = string.clone();
+                    for _ in 0..*n {
+                        new_string.push_str("bar");
+                    }
+                    output.push(new_string);
+                },
+            }
         }
         output
     }
@@ -44,13 +59,13 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    // 导入transformer函数
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
     fn it_works() {
-        let output = transformer(vec![
+        let output = transformer(&vec![
             ("hello".into(), Command::Uppercase),
             (" all roads lead to rome! ".into(), Command::Trim),
             ("foo".into(), Command::Append(1)),
