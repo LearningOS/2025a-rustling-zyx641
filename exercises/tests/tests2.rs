e8673a655ce92a2bfbfb39347e7b3d079f5013b4
@jade-seal-of-wisdom <3027210896@qq.com>
2025-09-23 22:02:04 +0800

    tests2

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -1,17 +1,19 @@
 // tests2.rs
 //
-// This test has a problem with it -- make the test compile! Make the test pass!
+// This test has a problem with it -- make the test compile! Make the test pass! 
 // Make the test fail!
 //
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        // 让测试通过的版本 (使用两个相等的值)
+        assert_eq!(2 + 2, 4);
+        
+        // 如果你想让测试失败，取消下面这行的注释
+        // assert_eq!(2 + 2, 5);
     }
 }
