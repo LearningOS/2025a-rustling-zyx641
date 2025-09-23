357509ffb7682d08d7b840ec98ffd86d737bc0a5
@jade-seal-of-wisdom <3027210896@qq.com>
2025-09-23 21:25:28 +0800

    move_semantics2

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -7,11 +7,13 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
+    vec0.push(22);
+    vec0.push(44);
+    vec0.push(66);
 
-    let mut vec1 = fill_vec(vec0.clone());
+    let mut vec1 = vec0.clone();
 
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
