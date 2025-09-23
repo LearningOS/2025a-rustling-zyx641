352778df845517a2888db02db81a2ad99a063d59
@jade-seal-of-wisdom <3027210896@qq.com>
2025-09-23 18:17:14 +0800

    8

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -1,6 +1,6 @@
 /*
 	queue
-	This question requires you to use queues to implement the functionality of the stac
+	This question requires you to use queues to implement the functionality of the stack
 */
 // I AM NOT DONE
 
@@ -54,28 +54,52 @@ impl<T> Default for Queue<T> {
 
 pub struct myStack<T>
 {
-	//TODO
-	q1:Queue<T>,
-	q2:Queue<T>
+	// 使用两个队列来实现栈
+	q1: Queue<T>,
+	q2: Queue<T>
 }
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
-			q1:Queue::<T>::new(),
-			q2:Queue::<T>::new()
+			// 初始化两个空队列
+			q1: Queue::<T>::new(),
+			q2: Queue::<T>::new()
         }
     }
     pub fn push(&mut self, elem: T) {
-        //TODO
+        // 将新元素添加到非空的队列中，如果都为空则添加到q1
+        if self.q1.is_empty() {
+            self.q2.enqueue(elem);
+        } else {
+            self.q1.enqueue(elem);
+        }
     }
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        // 如果两个队列都为空，返回错误
+        if self.q1.is_empty() && self.q2.is_empty() {
+            return Err("Stack is empty");
+        }
+        
+        // 确定哪个队列有元素，哪个队列为空
+        let (source, target) = if self.q1.is_empty() {
+            (&mut self.q2, &mut self.q1)
+        } else {
+            (&mut self.q1, &mut self.q2)
+        };
+        
+        // 将source队列中除了最后一个元素外的所有元素移动到target队列
+        while source.size() > 1 {
+            if let Ok(val) = source.dequeue() {
+                target.enqueue(val);
+            }
+        }
+        
+        // 返回并移除source队列中的最后一个元素（即栈顶元素）
+        source.dequeue()
     }
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+		// 栈为空当且仅当两个队列都为空
+        self.q1.is_empty() && self.q2.is_empty()
     }
 }
 
