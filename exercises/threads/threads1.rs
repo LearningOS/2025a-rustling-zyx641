c81f2259a85eaf7d12c54fff1ecaae7a2b663f8e
@jade-seal-of-wisdom <3027210896@qq.com>
2025-09-23 22:18:11 +0800

    threads1

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -8,8 +8,6 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -26,7 +24,8 @@ fn main() {
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+        // Use join() to wait for the thread to finish and get its return value
+        results.push(handle.join().unwrap());
     }
 
     if results.len() != 10 {
