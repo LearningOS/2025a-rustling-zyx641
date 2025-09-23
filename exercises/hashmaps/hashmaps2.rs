a9dd0f0fde2c892439e2e6065324f94eb1fb217f
@jade-seal-of-wisdom <3027210896@qq.com>
2025-09-23 20:20:58 +0800

    hashmaps2

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -37,9 +37,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
-        // basket. Note that you are not allowed to put any type of fruit that's
-        // already present!
+        // 只添加不在篮子里的水果
+        if !basket.contains_key(&fruit) {
+            basket.insert(fruit, 1);
+        }
     }
 }
 
