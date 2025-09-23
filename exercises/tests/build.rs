2da53fdb4baeddcc88f641db8db56217e4510802
@jade-seal-of-wisdom <3027210896@qq.com>
2025-09-23 22:11:30 +0800

    test7

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -1,5 +1,5 @@
 //! This is the build script for both tests7 and tests8.
-//!
+//! 
 //! You should modify this file to make both exercises pass.
 
 fn main() {
@@ -9,16 +9,13 @@ fn main() {
     let timestamp = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
-        .as_secs(); // What's the use of this timestamp here?
-    let your_command = format!(
-        "Your command here with {}, please checkout exercises/tests/build.rs",
-        timestamp
-    );
-    println!("cargo:{}", your_command);
+        .as_secs();
+    
+    // 设置 TEST_FOO 环境变量，使用 rustc-env 命令格式
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "Your command here, please checkout exercises/tests/build.rs";
-    println!("cargo:{}", your_command);
+    println!("cargo:rustc-cfg=feature=\"pass\"");
 }
